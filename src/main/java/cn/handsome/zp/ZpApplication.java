ade897a5c00b6a1a56af8819142a039f80f89570
ZhangPeng <29138386@qq.com>
2019-09-03 18:22:59 +0800

    项目初始化

---
+++ src/main/java/cn/handsome/zp/ZpApplication.java
@@ -0,0 +1,13 @@
+package cn.handsome.zp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZpApplication.class, args);
+    }
+
+}
